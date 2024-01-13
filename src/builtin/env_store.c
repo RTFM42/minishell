89ba996da731c11e73f9a70b76f8cd885845efa3
yukun <yukun0507@outlook.jp>
2024-01-13 18:00:03 +0900

    update

--- src/builtin/env_store.c
+++ src/builtin/env_store.c
@@ -6,20 +6,15 @@
 /*   By: yushsato <yushsato@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 17:50:07 by yushsato          #+#    #+#             */
-/*   Updated: 2024/01/13 17:54:34 by yushsato         ###   ########.fr       */
+/*   Updated: 2024/01/13 17:59:58 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../builtin.h"
 
-static t_env	**envstore()
+t_env	**envstore()
 {
 	static t_env	**chain;
 	
 	return (chain);
-}
-
-t_env	*env_getptr()
-{
-	return (*envstore());
 }
